f14d7662d98aea42259afe58a295744b40c996b4
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-08-22 01:43:49 -0700

    update main menu with engine menu

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Naturesoft::GlobalnaturesoftCom::Engine.routes.draw do
 end
 
 Naturesoft::Articles::Engine.routes.draw do
-    # Frontend
-    get "articles/:category/:id/:title.html" => "articles#detail", as: :detail_articles
-    get ":parent_title/:id/:title.html" => "categories#newest_article", as: :newest_article_categories
+    get "article/:id" => "articles#detail", as: :detail_articles
+    get "category/:id/newest-article" => "categories#newest_article", as: :newest_article_categories
+end
+
+Naturesoft::Projects::Engine.routes.draw do
+    get "portfolio/:category_id" => "projects#list", as: :list
+    get "portfolio/detail/:id" => "projects#detail", as: :projects_detail
 end
