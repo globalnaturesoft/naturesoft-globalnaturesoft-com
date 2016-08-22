05718ccf745abcd75699b08d2a3648491d053319
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-08-21 19:13:06 -0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Naturesoft::GlobalnaturesoftCom::Engine.routes.draw do
     root to: "home#index"
 end
+
+Naturesoft::Articles::Engine.routes.draw do
+    # Frontend
+    get "articles/:category/:id/:title.html" => "articles#detail", as: :detail_articles
+    get ":parent_title/:id/:title.html" => "categories#newest_article", as: :newest_article_categories
+end
