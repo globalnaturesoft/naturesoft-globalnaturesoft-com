c12c75c07b42e6187bc1d770e249a540dd3a59d5
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-08-23 21:15:08 -0700

    update module menu for engine _com

--- app/decorators/models/nsmodule_decorator.rb
+++ app/decorators/models/nsmodule_decorator.rb
@@ -5,13 +5,16 @@ Naturesoft::Nsmodule.class_eval do
     "footer-column-2" => nil,
     "footer-column-3" => nil,
     "footer-column-4" => nil,
+    "footer-top" => nil,
+    "top-bar" => nil,
+    "container-contact-page" => nil,
   }
   
   @globalnaturesoft_com = {
     "contact_info_box" => {
       "label" => "Contact Info Box",
       "options" => {
-        
+        "style" => "default"
       }
     }
   }
