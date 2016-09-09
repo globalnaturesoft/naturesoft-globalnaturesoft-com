8e748f7efe68839ffec6aaeec8576b909593deae
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-09-09 00:42:16 -0700

    update available for related engines

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,23 @@ Naturesoft::GlobalnaturesoftCom::Engine.routes.draw do
     root to: "home#index"
 end
 
-Naturesoft::Articles::Engine.routes.draw do
+if Naturesoft::Core.available?("articles")
+  Naturesoft::Articles::Engine.routes.draw do
     get "article/:id" => "articles#detail", as: :detail_articles
     get "category/:id/newest-article" => "categories#newest_article", as: :newest_article_categories
+  end
 end
-
-Naturesoft::Projects::Engine.routes.draw do
+if Naturesoft::Core.available?("projects")
+  Naturesoft::Projects::Engine.routes.draw do
     get "portfolio/:category_id" => "projects#list", as: :list
     get "portfolio/detail/:id" => "projects#detail", as: :projects_detail
+  end
 end
 
-Naturesoft::Contacts::Engine.routes.draw do
+if Naturesoft::Core.available?("contacts")
+  Naturesoft::Contacts::Engine.routes.draw do
     get "contact-us" => "contacts#contact", as: :contacts
     post "contact-us" => "contacts#contact"
     get "contact-us/success" => "contacts#success", as: :success
+  end
 end
